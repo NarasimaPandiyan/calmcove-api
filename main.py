284c07880601e08e5dcdaaf2e163658ee3cd0190
Narasima Pandiyan <35830653+NarasimaPandiyan@users.noreply.github.com>
2023-07-09 17:27:28 +0530

    Tweaking some stuff

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/")
-async def process_messages(data: list):
-    sender = []
-    for message in data:
-        role = message.get('role')
-        content = message.get('content')
-        sender.append(role)
-        
-    return {"message": str(sender)}
+@app.get("/v1/{Message}")
+async def process_messages(Message: string): 
+    return {"message": Message}
